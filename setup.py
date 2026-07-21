926d67d60cb409fa7a3ee27aa1ff846c7d657c9a
RAHUL-VERMA-CODE <145197758+RAHUL-VERMA-CODE@users.noreply.github.com>
2026-07-21 16:42:22 +0530

    change in setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,59 @@
-from setuptools import find_packages,setup
+# Import setup() and find_packages() from setuptools
+# setup() is used to package the project
+# find_packages() automatically finds all Python packages
+
+from setuptools import setup, find_packages
+
+# Import List for type hinting
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-     '''
-     This function will retuen the list of requirements
-     '''
-     requirements=[]
-     with open(file_path) as file_obj:
-          requirements=file_obj.readlines()
-          requirements=[req.replace("\n","") for req in requirements]
-          
-          if HYPEN_E_DOT in requirements:
-               requirements.remove(HYPEN_E_DOT)
-     return requirements           
+# This line is present inside requirements.txt
+# We don't want to install "-e ." as a dependency
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Reads the requirements.txt file
+    and returns a list of required packages.
+    """
+
+    requirements = []
+
+    # Open requirements.txt
+    with open(file_path) as file_obj:
+
+        # Read all lines
+        requirements = file_obj.readlines()
+
+        # Remove newline characters (\n)
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        # Remove "-e ." if present
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+# Package information
 setup(
-    name='MLproject',
-    version='0.0.1',
-    auther='Rahul',
-    author_email='rahulverma96259@gmail.com',
+
+    # Name of the package
+    name="MLproject",
+
+    # Package version
+    version="0.0.1",
+
+    # Author name
+    author="Rahul",
+
+    # Author email
+    author_email="rahulverma96259@gmail.com",
+
+    # Automatically find all packages
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+
+    # Install all dependencies from requirements.txt
+    install_requires=get_requirements("requirements.txt")
 )
